39288f7ad163e3c1110d4a156964a599e093aa6a
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 11:59:09 +0000

    Added answers

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import numpy as np
 def magnetisation( spins ) :
   mag = 0
   # Your code to calculate the magnetisation goes here
-  
+  mag = sum(spins)
   return mag
   
 def hamiltonian( spins, H ) :
   eng = 0
   # Your code to calculate the energy goes here
-  
+  eng = - spins[0]*spins[-1] - H*spins[0]
+  for i in range(1,len(spins)) : eng = eng - spins[i-1]*spins[i] - H*spins[i]
   return eng
   
 # Create a list of the posssible values the magnetisation can take
@@ -24,7 +25,17 @@ probabilities = 17*[0]
 # probabilities for the states with each particular magnetisation value as well as the 
 # canonical partition function goes here
 T, H, Z = 1.5, 0.2, 0
-
+for index in range(2**8) :
+  spins, ind = 8*[0], index
+  # Your code to convert the integer index to the corresponding spin coordinates goes here
+  for i in range(8) :
+      spins[i] = np.floor( index / 2**(7-i) )
+      index = index - spins[i]*(2**(7-i))
+      if spins[i]==0 : spins[i] = -1
+  nbin = int( magnetisation(spins) ) + 8
+  bweight = np.exp( -hamiltonian(spins,H)  / T ) 
+  probabilities[nbin] = probabilities[nbin] + bweight
+  Z = Z + bweight
 
 # Normalize the probabilities by dividing by the partition function
 for i in range(17) : probabilities[i] = probabilities[i] / Z
